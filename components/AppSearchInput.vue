8f97ec31fce5d90d6517fa284bcd7f1c7d36c96b
Nick <nick.mueller@uni-greifswald.de>
2020-11-08 19:42:36 +0100

    Adds projects and integrates to search

--- components/AppSearchInput.vue
+++ components/AppSearchInput.vue
@@ -4,19 +4,19 @@
       v-model="searchQuery"
       type="search"
       autocomplete="off"
-      placeholder="Search Articles"
+      placeholder="Search"
       class="block w-full pl-10 pr-3 py-2 truncate leading-5 placeholder-gray-500 border border-gray-500 text-gray-700 focus:border-gray-300 rounded-md focus:outline-none focus:bg-white bg-white"
     />
     <ul
-      v-if="articles.length"
+      v-if="items.length"
       class="z-10 absolute w-auto flex-1 top-40 bg-white dark:bg-gray-900 rounded-md border border-gray-300 overflow-hidden"
     >
-      <li v-for="article of articles" :key="article.slug">
+      <li v-for="item of items" :key="item.slug">
         <NuxtLink
-          :to="{ name: 'blog-slug', params: { slug: article.slug } }"
+          :to="{ name: item.slugType + '-slug', params: { slug: item.slug } }"
           class="flex px-4 py-2 items-center leading-5 transition ease-in-out duration-150 text-green-500 hover:text-black"
         >
-          {{ article.title }}
+          {{ item.title }}
         </NuxtLink>
       </li>
     </ul>
@@ -27,19 +27,30 @@ export default {
   data() {
     return {
       searchQuery: '',
-      articles: []
+      items: []
     }
   },
   watch: {
     async searchQuery(searchQuery) {
       if (!searchQuery) {
-        this.articles = []
+        this.items = []
         return
       }
-      this.articles = await this.$content('articles')
+      const articles = await this.$content('articles')
         .limit(6)
         .search(searchQuery)
         .fetch()
+
+      articles.forEach((a) => (a.slugType = 'blog'))
+
+      const projects = await this.$content('projects')
+        .limit(6)
+        .search(searchQuery)
+        .fetch()
+
+      projects.forEach((p) => (p.slugType = 'projects'))
+
+      this.items = [...projects, ...articles]
     }
   }
 }
