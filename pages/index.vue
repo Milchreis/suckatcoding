4f12acc59061421a6a510ec5581bca9b577adb3b
Nick <nick.mueller@uni-greifswald.de>
2020-11-08 19:46:28 +0100

    Footer

--- pages/index.vue
+++ pages/index.vue
@@ -50,18 +50,9 @@
     <footer class="flex justify-center border-gray-500 border-t-2">
       <p class="mt-4">
         Created by
-        <a
-          href="https://twitter.com/debs_obrien"
-          class="font-bold hover:underline"
-          >Debbie O'Brien</a
+        <a href="https://github.com/milchreis" class="font-bold hover:underline"
+          >Nick Müller</a
         >
-        at NuxtJS. See the
-        <a
-          href="https://nuxtjs.org/blog/creating-blog-with-nuxt-content"
-          class="font-bold hover:underline"
-          >tutorial</a
-        >
-        for how to build it.
       </p>
     </footer>
   </div>
