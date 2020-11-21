188066c9ac81f32c772e64851ab558486490257b
Nick <nick.mueller@uni-greifswald.de>
2020-11-21 18:45:11 +0100

    Moves ToC to separate component

---
+++ components/TableOfContent.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="pb-6">
+    <ul>
+      <li
+        v-for="link of article.toc"
+        :key="link.id"
+        :class="{
+          'font-semibold': link.depth === 2
+        }"
+      >
+        <nuxtLink
+          :to="`#${link.id}`"
+          class="hover:underline"
+          :class="{
+            'py-2': link.depth === 2,
+            'ml-2 pb-2': link.depth === 3
+          }"
+          >{{ link.text }}</nuxtLink
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
