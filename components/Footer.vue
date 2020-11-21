de6faaecb43e14e70abd2caebe24fe79b07c2cf9
Nick <nick.mueller@uni-greifswald.de>
2020-11-21 18:43:21 +0100

    Adds footer

---
+++ components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <div class="container">
+      <Face />
+      <Logo />
+    </div>
+  </footer>
+</template>
+
+<style lang="scss">
+footer {
+  background-color: #222;
+
+  .container {
+    display: flex;
+    justify-content: center;
+
+    img {
+      max-height: 150px;
+      padding: 2em 0 2em 0;
+    }
+  }
+}
+</style>
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -18,8 +18,8 @@
             <span class="mr-3">•</span>
             <p>{{ article.author.name }}</p>
           </div>
-          <h1 class="text-6xl font-bold">{{ article.title }}</h1>
-          <p class="-mt-2 mb-5">{{ article.description }}</p>
+          <h1 class="text-6xl font-extrabold">{{ article.title }}</h1>
+          <p class="-mt-2 mb-5 font-bold">{{ article.description }}</p>
           <span v-for="(tag, id) in article.tags" :key="id">
             <NuxtLink :to="`/blog/tag/${tags[tag].slug}`">
               <span
@@ -49,39 +49,24 @@
       <div
         class="container relative xs:py-8 xs:px-8 lg:py-8 xs:w-full markdown-body post-right custom-scroll"
       >
-        <!-- <h1 class="font-bold text-4xl">{{ article.title }}</h1>
-        <p>{{ article.description }}</p>
-        <p class="pb-4">
-          Post last updated: {{ formatDate(article.updatedAt) }}
-        </p> -->
         <!-- table of contents -->
-        <nav class="pb-6">
-          <ul>
-            <li
-              v-for="link of article.toc"
-              :key="link.id"
-              :class="{
-                'font-semibold': link.depth === 2
-              }"
-            >
-              <nuxtLink
-                :to="`#${link.id}`"
-                class="hover:underline"
-                :class="{
-                  'py-2': link.depth === 2,
-                  'ml-2 pb-2': link.depth === 3
-                }"
-                >{{ link.text }}</nuxtLink
-              >
-            </li>
-          </ul>
-        </nav>
+        <TableOfContent v-if="false" :article="article"></TableOfContent>
+
         <!-- content from markdown -->
         <nuxt-content :document="article" />
+      </div>
+
+      <div class="container relative xs:py-8 xs:px-8 lg:py-8 xs:w-full">
         <!-- content author component -->
         <author :author="article.author" />
-        <!-- prevNext component -->
-        <PrevNext :prev="prev" :next="next" class="mt-8" />
+      </div>
+
+      <div class="sectionDarker">
+        <div class="container relative xs:py-8 xs:px-8 lg:py-8 xs:w-full">
+          <h1 class="text-3xl font-extrabold">Other articles</h1>
+          <!-- prevNext component -->
+          <PrevNext :prev="prev" :next="next" class="mt-8" />
+        </div>
       </div>
     </article>
   </div>
@@ -115,7 +100,41 @@ export default {
   }
 }
 </script>
-<style>
+
+<style lang="scss">
+.sectionDarker {
+  color: #ebebeb;
+  background-color: #444;
+  padding: 2em;
+}
+
+article {
+  font-size: 1.1rem;
+  h2 {
+    font-size: 2.5rem !important;
+    font-weight: 800 !important;
+    margin-bottom: 1rem;
+    line-height: 0.95;
+  }
+  h3 {
+    font-size: 2rem !important;
+    font-weight: 800 !important;
+    margin-bottom: 0.6rem;
+    line-height: 0.95;
+  }
+  h4 {
+    font-size: 1.7rem !important;
+    font-weight: 800 !important;
+    margin-bottom: 0.4rem;
+    line-height: 0.95;
+  }
+  pre code {
+    font-size: 0.85em;
+    line-height: 1.2em;
+    letter-spacing: 0;
+  }
+}
+
 .markdown-body .nuxt-content a {
   color: #d31864;
   text-decoration: inherit;
@@ -140,6 +159,8 @@ export default {
   width: 100%;
   height: 100%;
   filter: brightness(75%) blur(5px);
+  background-size: cover;
+  background-repeat: no-repeat;
 }
 
 .overlay {
