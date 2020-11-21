5b34e88ffd5cdf87c2eaa46ac789001a9bbda5ec
Nick <nick.mueller@uni-greifswald.de>
2020-11-21 18:44:22 +0100

    Adds arrows

--- components/global/PrevNext.vue
+++ components/global/PrevNext.vue
@@ -5,15 +5,44 @@
       :to="{ name: 'blog-slug', params: { slug: prev.slug } }"
       class="text-primary font-bold hover:underline"
     >
-      {{ prev.title }}
+      <span>
+        <svg
+          width="10px"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        {{ prev.title }}
+      </span>
     </NuxtLink>
     <span v-else>&nbsp;</span>
+
     <NuxtLink
       v-if="next"
       :to="{ name: 'blog-slug', params: { slug: next.slug } }"
       class="font-bold hover:underline"
     >
-      {{ next.title }}
+      <span>
+        {{ next.title }}
+        <svg
+          width="10px"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </span>
     </NuxtLink>
     <span v-else>&nbsp;</span>
   </div>
@@ -33,3 +62,10 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+img,
+svg {
+  display: inline-block;
+}
+</style>
