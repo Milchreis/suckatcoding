5b5720b96d1e96661ceeba87c2e2ed6955faaba7
Milchreis <nick-m@gmx.de>
2021-02-11 22:38:37 +0100

    Updates the author section

--- components/Author.vue
+++ components/Author.vue
@@ -1,21 +1,16 @@
 <template>
-  <div class="w-full px-2 xs:mb-6 md:mb-12 article-card">
-    <NuxtLink
-      :to="`/blog/author/${author.name}`"
-      class="flex transition-shadow duration-150 ease-in-out shadow-sm hover:shadow-md xxlmax:flex-col"
-    >
+  <div class="w-1/2 px-2 xs:mb-6 md:mb-12 mx-auto">
+    <div class="flex transition-shadow xxlmax:flex-col">
       <img
         v-if="author.img"
-        class="h-48 xxlmin:w-1/2 xxlmax:w-full object-cover"
+        class="h-32 xxlmin:w-1/2 xxlmax:w-full object-cover rounded-full"
         :src="author.img"
       />
-      <div class="flex flex-col m-4">
-        <h4 class="font-semibold">Author</h4>
-
-        <p>{{ author.name }}</p>
-        <p>{{ author.bio }}</p>
+      <div class="flex flex-col ml-4">
+        <h4 class="font-semibold text-gray-900">{{ author.name }}</h4>
+        <p class="text-gray-700 italic text-md">{{ author.bio }}</p>
       </div>
-    </NuxtLink>
+    </div>
   </div>
 </template>
 
